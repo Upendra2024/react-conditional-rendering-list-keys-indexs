e538c431880e7df4fa787833f3cc4409e910f2df
Upendra2024 <92069507+Upendra2024@users.noreply.github.com>
2022-09-24 22:34:13 +0530

    1 file modified

--- src/NameList.jsx
+++ src/NameList.jsx
@@ -1,40 +1,34 @@
 import React from 'react';
-import {Person} from './Person'
+import { Person } from './Person';
 
-export const NameList  =() =>{
- const  persons =[
+export const NameList = () => {
+  const persons = [
     {
-      id :1,
-      name:'Upendra',
-      age :25,
-      skill :'react'
-
+      id: 1,
+      name: 'Upendra',
+      age: 25,
+      skill: 'react',
     },
     {
-      id :2,
-      name:'Surya',
-      age :24,
-      skill :'html'
-
+      id: 2,
+      name: 'Surya',
+      age: 24,
+      skill: 'html',
     },
     {
-      id :3,
-      name:'Pujitha',
-      age :19,
-      skill :'java'
-
+      id: 3,
+      name: 'Pujitha',
+      age: 19,
+      skill: 'java',
     },
     {
-      id :4,
-      name:'Teja',
-      age :20,
-      skill :'python'
+      id: 4,
+      name: 'Teja',
+      age: 20,
+      skill: 'python',
+    },
+  ];
 
-    }
-  ]
-
-  const PersonList = persons.map(person => <Person person = {person}/>)
-  return(
-<div> {PersonList}</div>
-  )
-}
+  const PersonList = persons.map((person) => <Person key = {person.id} person={person} />);
+  return <div> {PersonList}</div>;
+};
