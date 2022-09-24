19fa2693e78ffd560eba3fafe59735b55bde43af
Upendra2024 <92069507+Upendra2024@users.noreply.github.com>
2022-09-24 21:59:22 +0530

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.css";
 import {UserGreet} from './UserGreet';
-
+import {NameList} from './NameList';
 export default function App() {
   return (
     <div>
       <UserGreet/>
+      <NameList/>
       
     </div>
   );
---
+++ src/NameList.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Person} from './Person'
+
+export const NameList  =() =>{
+ const  persons =[
+    {
+      id :1,
+      name:'Upendra',
+      age :25,
+      skill :'react'
+
+    },
+    {
+      id :2,
+      name:'Surya',
+      age :24,
+      skill :'html'
+
+    },
+    {
+      id :3,
+      name:'Pujitha',
+      age :19,
+      skill :'java'
+
+    },
+    {
+      id :4,
+      name:'Teja',
+      age :20,
+      skill :'python'
+
+    }
+  ]
+
+  const PersonList = persons.map(person => <Person person = {person}/>)
+  return(
+<div> {PersonList}</div>
+  )
+}
